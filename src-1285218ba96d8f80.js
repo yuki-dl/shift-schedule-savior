7897ca6543cea5994d8d132e05aabfd37b78d681
yuki-dl <yuki-dl@users.noreply.github.com>
2023-09-29 09:52:18 +0000

    deploy: 5237c7c5f6d0ab2c4a80c8dac641fb8049016c5c

--- src-1285218ba96d8f80.js
+++ src-1285218ba96d8f80.js
@@ -1212,7 +1212,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('src-e58f0eaf0fe21451_bg.wasm', import.meta.url);
+        input = new URL('src-1285218ba96d8f80_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
